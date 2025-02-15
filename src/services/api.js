de6ffb3d636bec393ac98a0b2d79f0bd17bb4c83
Harsh Narayan Jha <harshnj@proton.me>
2025-02-15 12:15:45 +0530

    add basic syllabuses

--- src/services/api.js
+++ src/services/api.js
@@ -3,14 +3,10 @@ import { useFetch } from '@vueuse/core'
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export const fetchSubjects = async year => {
-	console.log('Fetching from', API_BASE_URL)
-	const { data, error, isFetching } = await useFetch(API_BASE_URL, {
+	console.log('Fetching from', `${API_BASE_URL}/pyq/year/${year}`)
+	const { data, error, isFetching } = await useFetch(`${API_BASE_URL}/pyq/year/${year}`, {
 		refetch: false,
 		initialData: [],
-		afterFetch(ctx) {
-			ctx.data = ctx.data[year]
-			return ctx
-		},
 		onFetchError(ctx) {
 			console.log('Error fetching subjects list: ', ctx.error)
 			return ctx
@@ -21,7 +17,7 @@ export const fetchSubjects = async year => {
 }
 
 export const fetchSyllabus = async code => {
-  const { data, error, isFetching } = await useFetch(`${API_BASE_URL}/syllabus/${code}`, {
+  const { data, error, isFetching } = await useFetch(`${API_BASE_URL}/syllabus/code/${code}`, {
     refetch: false,
     initialData: "",
     afterFetch(ctx) {
