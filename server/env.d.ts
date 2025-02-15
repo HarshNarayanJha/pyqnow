84c8f4516e95f460c67f7007185c219bbb93e204
Harsh Narayan Jha <harshnj@proton.me>
2025-02-15 13:49:47 +0530

    add server env types

--- server/env.d.ts
+++ server/env.d.ts
@@ -3,8 +3,9 @@ declare global {
 		interface ProcessEnv {
 			NODE_ENV: 'development' | 'production' | 'test'
 			PORT: string
-			PYQ_JSON_URL: string,
-			SYLLABUS_JSON_URL: string,
+			PYQ_JSON_URL: string
+			SYLLABUS_JSON_URL: string
+			HOST_URL: string
 		}
 	}
 }
