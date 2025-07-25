9106d72c7db0fab27fa5af86b1a40b09935e8ddf
Harsh Narayan Jha <harshnj@proton.me>
2025-07-25 16:45:53 +0530

    add missing 4 dir to server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url"
+import vue from "@vitejs/plugin-vue"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,15 +8,15 @@ export default defineConfig({
     vue({
       template: {
         compilerOptions: {
-          isCustomElement: (tag) => tag.includes('-'),
-          types: ["@shoelace-style/shoelace/dist/types/vue"]
-        }
-      }
+          isCustomElement: tag => tag.includes("-"),
+          types: ["@shoelace-style/shoelace/dist/types/vue"],
+        },
+      },
     }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
