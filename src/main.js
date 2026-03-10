4784753fca45246ceeead97f187a7f6340cdbedb
Harsh Narayan Jha <harshnj@proton.me>
2026-03-10 15:09:12 +0530

    create proxy routes on server for umami
    - breaking: removes `/api` trailing route from the env var on frontend
    - set cache control headers on the server
    - add new `/health` route
    - analytic routes are `/stats/js` and `/stats/event`

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path"
 setBasePath("https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.16.0/cdn/")
 
 const UMAMI_WEBSITE_ID = import.meta.env.VITE_UMAMI_WEBSITE_ID
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 const app = createApp(App)
 
@@ -17,10 +18,11 @@ if (UMAMI_WEBSITE_ID !== "") {
   app.use(
     VueUmamiPlugin({
       websiteID: UMAMI_WEBSITE_ID,
-      scriptSrc: "https://cloud.umami.is/script.js",
+      scriptSrc: `${API_BASE_URL}/stats/js`,
       router,
       allowLocalhost: false,
       extraDataAttributes: {
+        "data-host-url": `${API_BASE_URL}/stats/events`,
         "data-do-not-track": true,
         "data-auto-track": true,
       },
